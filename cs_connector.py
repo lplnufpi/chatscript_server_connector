db8395e02422b21113ed0a6d3e94d17ea93553a2
José Patrício <jpegx100@gmail.com>
2019-10-22 12:00:34 -0300

    Docstrings

--- cs_connector.py
+++ cs_connector.py
@@ -2,13 +2,33 @@ import socket
 
 
 class CSConnection(object):
+    """Class to connect and send messages to ChatScript Server."""
+
     def __init__(self, user, botname='harry', ip='localhost', port=1024):
+        """Init the connection.
+
+        Args:
+            user (str): Username that will be sended in every message.
+            botname (str): Chatbot that will be used. Default 'harry'.
+            ip (str): IP address to ChatScript server. Default
+                'localhost'.
+            port (int): Port number to ChatScript server. Default
+                '1024'.
+        """
         self.user = user
         self.ip = ip
         self.port = port
         self.botname = botname
 
     def send(self, message):
+        """This method is used to send message to the bot.
+
+        Args:
+            message (str): Message to send.
+
+        Returns:
+            str: Returned message from the bot.
+        """
         msg = '%s\u0000%s\u0000%s\u0000' % (self.user, self.botname, message)
         msg = str.encode(msg)
 
@@ -25,6 +45,6 @@ class CSConnection(object):
 
 
 if __name__ == '__main__':
-    conn = CSConnection('jp')
-    msg = conn.send('scare')
+    conn = CSConnection('username')
+    msg = conn.send('Hello')
     print('>>>> {}'.format(msg))
